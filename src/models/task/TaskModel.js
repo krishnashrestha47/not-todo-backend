b02af06e94461131f5be0057cd019b576b5f2637
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-07-13 22:22:23 +1000

    queries created for the task

---
+++ src/models/task/TaskModel.js
@@ -0,0 +1,26 @@
+import TaskSchema from "./TaskSchema";
+
+//insert
+export const insertTask = (taskObj) => {
+  return TaskSchema(taskObj).save();
+};
+
+//select
+
+export const getTasks = () => {
+  return TaskSchema.find();
+};
+
+export const getSingleTask = (_id) => {
+  return TaskSchema.findById(_id);
+};
+
+// update
+export const updatTask = (_id, type) => {
+  return TaskSchema.findByIdAndUpdate(_id, { type }, { new: true });
+};
+
+// delete single item by id
+export const deleteTaskById = (_id) => {
+  return TaskSchema.findByIdAndDelete(_id);
+};
